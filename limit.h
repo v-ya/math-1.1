42dd383245eb9b85594fac483fe25028bd3e04d2
v-ya <sinosakisino5@gmail.com>
2019-04-04 18:07:14 +0800

    :(

--- limit.h
+++ limit.h
@@ -7,7 +7,7 @@
 // 版本号
 	#define	VERSION_major		1
 	#define VERSION_minor		7
-	#define VERSION_revision	1
+	#define VERSION_revision	2
 	#define VERSION_value		(VERSION_major<<16|VERSION_minor<<8|VERSION_revision)
 
 // 限制
